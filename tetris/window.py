524e04098808f1ca3fc15807dda313bc076df82f
Almaas <jonasalmaas7@gmail.com>
2021-01-10 19:56:16 +0100

    + window icon

--- tetris/window.py
+++ tetris/window.py
@@ -1,10 +1,14 @@
 import pygame
+from pathlib import Path
 
 
 class Window:
     def __init__(self, title: str = 'Title', width: int = 900, height: int = 700):
         pygame.init()
 
+        icon = pygame.image.load(str(Path(__file__).parent / 'images/icon.png'))
+
         pygame.display.set_caption(title)
+        pygame.display.set_icon(icon)
 
         self.display = pygame.display.set_mode((width, height), pygame.RESIZABLE)
